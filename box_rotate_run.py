23443371419a0aeee3718ede77f62965369cefc7
kjh980610 <129719906+kjh980610@users.noreply.github.com>
2023-04-19 16:23:02 +0900

    Update box_rotate_run.py

--- box_rotate_run.py
+++ box_rotate_run.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import box_rotate
+import target_rotate
 from pykin.kinematics.transform import Transform
 import numpy as np
 
 
 current_pose = Transform(pos=np.array([0,0,0]), rot=np.array([0.0, 0.0, 0.0]))
-target_pose = Transform(pos=np.array([0,0,0]), rot=np.array([0.0, 0.0, np.pi/2]))
+target_pose = Transform(pos=np.array([0,0,0]), rot=np.array([0.0, 0.0, np.pi]))
 
-
-box_rotate.box_rotate(current_pose,target_pose)
+target_rotate.target_rotate(current_pose,target_pose)
